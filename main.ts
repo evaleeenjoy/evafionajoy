aa23767fd487500cc3d2a6a8b6359d6eca4a8729
evaleeenjoy <105703903+evaleeenjoy@users.noreply.github.com>
2022-05-17 17:57:41 +0800

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,93 +1,95 @@
-input.onButtonPressed(Button.A, function () {
-    basic.clearScreen()
-    basic.showString("Start!")
-    basic.showLeds(`
-        # . . . .
-        . . . . .
-        . . . . .
-        . . . . .
-        . . . . .
-        `)
-    basic.showLeds(`
-        . # . . .
-        # . . . .
-        . . . . .
-        . . . . .
-        . . . . .
-        `)
-    basic.showLeds(`
-        . . # . .
-        . # . . .
-        # . . . .
-        . . . . .
-        . . . . .
-        `)
-    basic.showLeds(`
-        . . . # .
-        . . # . .
-        . # . . .
-        # . . . .
-        . . . . .
-        `)
-    basic.showLeds(`
-        . . . . #
-        . . . # .
-        . . # . .
-        . # . . .
-        # . . . .
-        `)
-    basic.showLeds(`
-        . . . . .
-        . . . . #
-        . . . # .
-        . . # . .
-        . # . . .
-        `)
-    basic.showLeds(`
-        . . . . .
-        . . . . .
-        . . . . #
-        . . . # .
-        . . # . .
-        `)
-    basic.showLeds(`
-        . . . . .
-        . . . . .
-        . . . . .
-        . . . . #
-        . . . # .
-        `)
-    basic.showLeds(`
-        . . . . .
-        . . . . .
-        . . . . .
-        . . . . .
-        . . . . #
-        `)
-    basic.pause(100)
-})
-input.onButtonPressed(Button.B, function () {
-    if (LIGHT < 255) {
-        for (let index = 0; index < 17; index++) {
-            LIGHT = LIGHT + 15
-            basic.pause(100)
-        }
-    }
-    if (LIGHT == 255) {
-        for (let index = 0; index < 17; index++) {
-            LIGHT = LIGHT - 15
-            basic.pause(100)
-        }
-    }
-})
 let LIGHT = 0
-basic.showLeds(`
-    . # . # .
-    # # # # #
-    # # # # #
-    . # # # .
-    . . # . .
-    `)
 basic.forever(function () {
-    led.setBrightness(LIGHT)
+    if (input.buttonIsPressed(Button.B)) {
+        led.setBrightness(LIGHT)
+    } else {
+        led.setBrightness(255)
+    }
+})
+basic.forever(function () {
+    if (input.buttonIsPressed(Button.B)) {
+        basic.showIcon(IconNames.Heart)
+        if (LIGHT < 255) {
+            for (let index = 0; index < 17; index++) {
+                LIGHT = LIGHT + 15
+                basic.pause(100)
+            }
+        }
+        if (LIGHT == 255) {
+            for (let index = 0; index < 17; index++) {
+                LIGHT = LIGHT - 15
+                basic.pause(100)
+            }
+        }
+        basic.clearScreen()
+    } else {
+        if (input.buttonIsPressed(Button.A)) {
+            basic.showString("Start!")
+            basic.showLeds(`
+                # . . . .
+                . . . . .
+                . . . . .
+                . . . . .
+                . . . . .
+                `)
+            basic.showLeds(`
+                . # . . .
+                # . . . .
+                . . . . .
+                . . . . .
+                . . . . .
+                `)
+            basic.showLeds(`
+                . . # . .
+                . # . . .
+                # . . . .
+                . . . . .
+                . . . . .
+                `)
+            basic.showLeds(`
+                . . . # .
+                . . # . .
+                . # . . .
+                # . . . .
+                . . . . .
+                `)
+            basic.showLeds(`
+                . . . . #
+                . . . # .
+                . . # . .
+                . # . . .
+                # . . . .
+                `)
+            basic.showLeds(`
+                . . . . .
+                . . . . #
+                . . . # .
+                . . # . .
+                . # . . .
+                `)
+            basic.showLeds(`
+                . . . . .
+                . . . . .
+                . . . . #
+                . . . # .
+                . . # . .
+                `)
+            basic.showLeds(`
+                . . . . .
+                . . . . .
+                . . . . .
+                . . . . #
+                . . . # .
+                `)
+            basic.showLeds(`
+                . . . . .
+                . . . . .
+                . . . . .
+                . . . . .
+                . . . . #
+                `)
+            basic.pause(100)
+            basic.clearScreen()
+        }
+    }
 })
